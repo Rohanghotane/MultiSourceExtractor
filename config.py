92fbbf8a9c4faeb3671d4ae5dc3bacb74bd177d6
Rohan Ghotane <145056559+Rohanghotane@users.noreply.github.com>
2025-10-16 16:47:33 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+OUTPUT_XLSX = "extracted_results.xlsx"
+
+URL_GRAYPOINT = "https://www.gray-point.com/properties/"
+URL_RIGHTMOVE = "https://www.rightmove.co.uk/commercial-property-to-let.html"
+AMAZON_BASE = "https://www.amazon.in"
+INSTAGRAM_BASE = "https://www.instagram.com"
+
+TESSERACT_CMD = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
